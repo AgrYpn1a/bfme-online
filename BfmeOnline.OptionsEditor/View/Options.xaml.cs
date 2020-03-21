03541b8048074b07c5158d0817c8e5ef65ff0e46
rtojagic <tojagic.rastko@gmail.com>
2020-03-21 21:57:04 +0100

    Removed reduntant graphics and reduced graphics size; Added separate project for Options ini editor;

---
+++ BfmeOnline.OptionsEditor/View/Options.xaml.cs
@@ -0,0 +1,34 @@
+﻿using BfmeOnline.OptionsEditor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BfmeOnlineLauncher.View
+{
+    /// <summary>
+    /// Interaction logic for Options.xaml
+    /// </summary>
+    public partial class Options : Window
+    {
+        public Options()
+        {
+            InitializeComponent();
+            DataContext = this;
+            PopulateComboboxFromEnum();
+        }
+
+        private void PopulateComboboxFromEnum()
+        {
+            cbResoultion.ItemsSource = Enum.GetValues(typeof(Resolution)).Cast<Resolution>().ToList()?.Select(s => s.ToDescriptionString());
+        }
+    }
+}
